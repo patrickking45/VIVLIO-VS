a772abbc303cd713d879bf867f11aec021cbac2e
Patrick Dufour <patrick.dufour45@gmail.com>
2016-05-05 20:11:28 -0400

    VIVLIO.PD.2.2016-05-05

---
+++ VIVLIO/VIVLIO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VIVLIO.Startup))]
+namespace VIVLIO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
